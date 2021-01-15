3092d3020db949b872501a93323323674c1f2ff2
ferial71 <ff_bouzoul@esi.dz>
2021-01-15 15:20:27 +0100

    created database

---
+++ database/migrations/2021_01_05_235835_create_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Tenant;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->foreignIdFor(Tenant::class);
+            $table->foreignIdFor(Tenant::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
