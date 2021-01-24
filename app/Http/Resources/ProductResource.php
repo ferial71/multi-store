bad8711e935517b76ef56e4811fa312db266ab4f
ferial71 <ff_bouzoul@esi.dz>
2021-01-24 21:36:28 +0100

    seeders factories models

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'description' => $this->description,
+            'price' => $this->price,
+            'weight' => $this->weight,
+            'image'=>$this->image,
+            'location' => $this->location,
+            'category_id' => $this->category_id,
+            'stock' => $this->stock,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
