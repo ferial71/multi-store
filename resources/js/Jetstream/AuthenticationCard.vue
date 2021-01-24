64ec39f97baea1e4082fbce787b79fd83fa7b364
ferial71 <ff_bouzoul@esi.dz>
2021-01-24 21:19:31 +0100

    added devcloud logo

--- resources/js/Jetstream/AuthenticationCard.vue
+++ resources/js/Jetstream/AuthenticationCard.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
         <div>
-            <slot name="logo" />
+            <jet-application-mark class="block h-20 w-20" />
         </div>
 
         <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
@@ -9,3 +9,11 @@
         </div>
     </div>
 </template>
+<script>
+    import JetApplicationMark from '@/Jetstream/ApplicationMark'
+    export default {
+        components: {
+            JetApplicationMark,
+        }
+    }
+</script>
