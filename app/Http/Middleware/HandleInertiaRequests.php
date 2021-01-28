970972b7fbe30a03a8afe664ddc2205792032981
ferial71 <ff_bouzoul@esi.dz>
2021-01-29 00:56:41 +0100

    send cart info to all application

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 
@@ -37,7 +38,8 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request)
     {
         return array_merge(parent::share($request), [
-            //
+            'cartCount' => Cart::count(),
+            'cartContent' => Cart::content(),
         ]);
     }
 }
